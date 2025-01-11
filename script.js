995ae1b152247852719f9b8074694aa255555239
Nayan Pillewar <nayanpillewar7@gmail.com>
2025-01-11 14:37:48 +0530

    htmlcssjs

---
+++ script.js
@@ -0,0 +1,26 @@
+// alert("Welcome to the sigma web devlopment course")
+// console.log("Code  is looking like a wow")
+// var a=prompt("Enter your number")
+// var isTrue = confirm("Are you sure you want to leave this page and blast your computer")
+// if(isTrue){
+//     console.log("Computer is blasting")
+// }else{
+//     console.log("Computer is not blasting")
+// }
+// console.log("your no is "+a)
+
+// document.title= "hey i am good"
+// document.body.style.backgroundColor = "red"
+var a=5;
+var b=6;
+var c="Nayan";
+console.log(a+b+8)
+console.log(typeof a, typeof b, typeof c)
+
+let age =18;
+if (age===18){
+    console.log("you can drive");
+}
+else {
+    console.log("you can't drive");
+}
