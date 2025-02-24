60f83687fa0e880891c211d928c83dd4b6f2fdaf
Nayan Pillewar <nayanpillewar7@gmail.com>
2025-02-24 15:48:18 +0530

    Packeg.js

--- main.js
+++ main.js
@@ -1,4 +1,33 @@
 var a=5;
 var b=6;
 var ans= a+b;
+const name='Nayan';
 console.log("our ans is :" + ans);
+console.log(typeof ans);
+console.log(typeof name);
+
+
+const cars=["nayan","mahi","atharva"];
+cars.push("BMW");
+console.log(cars);
+console.log(cars[2]);
+
+//Object
+const person={
+    name: "Nayan Pillewar",
+    age: 19,
+    isStudent: true,
+    hobbies: ["Painting","Dancing"]
+};
+console.log(person.hobbies);
+//          key    values
+
+//Filter function
+const age= prompt("Please enter your age");
+const result =ages.filter(cheakAge);
+
+function cheakAge(age){
+    return age>=18;
+}
+console.log(result);
+
