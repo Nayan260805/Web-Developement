3187c170c5a87c8aef98b8901af6ca3d595682e5
Nayan Pillewar <nayanpillewar7@gmail.com>
2025-02-24 23:19:58 +0530

    Functions.js

---
+++ server.js
@@ -0,0 +1,39 @@
+// function add(a,b){
+//     return a+b;
+// }
+
+// var add = function(a,b){
+//     return a+b;
+// }
+
+// var result = add(2,5);
+// console.log(result);
+
+// (function(){
+//     console.log('prince');
+// })();
+
+//call back function
+// function callback(){
+//     console.log('now adding is successful complete');
+// }
+
+// const add =function(a,b,callback){
+//     var result =a+b;
+//     console.log('result: '+result);
+//     callback();
+// }
+
+// add(3,100,callback)
+
+const add =function(a,b,prince){
+    var result =a+b;
+    console.log('result: '+result);
+    prince();
+}
+
+// add(2,3,function(){
+//     console.log('add completed');
+// });
+
+add(2,3, ()=>console.log('add completed'));
