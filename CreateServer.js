638e32f0c021f7bd39c5e84dc8948d1ac337eb82
Nayan Pillewar <nayanpillewar7@gmail.com>
2025-03-11 00:40:03 +0530

    libraries bcrypt

--- CreateServer.js
+++ CreateServer.js
@@ -2,8 +2,9 @@ const express= require('express')
 const app=express();
 const db=require('./db');
 require('dotenv').config();
-const passport=require('passport');
-const LocalStrategy= require('passport-local').Strategy;
+const passport=require('./auth');
+
+
 //const person=require('./models/person');
 
 
@@ -17,35 +18,17 @@ const logRequest=(req,res,next)=>{
     console.log(`[${new Date().toLocaleString()}] Request Made to: ${req.originalUrl}`);
     next();//function is the callback that signals to Express that the current middleware function has completed its processing and it's time to move on to the next middleware function or route handler in the chain 
 }
-
+app.use(logRequest);
 // const person = require('./models/person');
 // const Menu = require('./models/Menu');
 //GET
-app.use(logRequest);// line teels Express to use this middleware for all routes
-passport.use(new LocalStrategy(async(USERNAME,password,done)=>{
-    //authentication logic here 
-    try{
-        console.log('Received Credentials:',USERNAME,password);
-        const user=await person.findOne({username: USERNAME});
-        if(!user){
-            return done(null,false,{message:'Incorrect username.'});
-        }
-        const isPasswordMatch=user.password===password? true : false;
-        if(isPasswordMatch){
-            return done(null,user);
-        }else{
-            return done (null,false,{message: 'Incorrect password.'});
-        }
-    }catch(err){
-        return done(err);
-    }
-}))
+
 
 
 app.use(passport.initialize());
 
 const localAuthMiddleware=passport.authenticate('local',{session: false})
-app.get('/',localAuthMiddleware,function(req,res){
+app.get('/',function(req,res){
     res.send('hello world Nayan');
 })
 
@@ -62,8 +45,8 @@ app.get('/',localAuthMiddleware,function(req,res){
 const personRouter =require('./Routes/personRouter');
 const menuRoutes =require('./Routes/menuRoutes');
 const person = require('./models/person');
-app.use('/person' ,personRouter);
-app.use('/Menu' ,localAuthMiddleware,menuRoutes);
+app.use('/person' ,localAuthMiddleware,personRouter);
+app.use('/Menu' ,menuRoutes);
 
 
 app.listen(PORT,()=>{
---
+++ auth.js
@@ -0,0 +1,25 @@
+const passport=require('passport');
+const LocalStrategy= require('passport-local').Strategy;
+const person=require('./models/person');
+
+//app.use(logRequest);// line teels Express to use this middleware for all routes
+passport.use(new LocalStrategy(async(USERNAME,password,done)=>{
+    //authentication logic here 
+    try{
+        console.log('Received Credentials:',USERNAME,password);
+        const user=await person.findOne({username: USERNAME});
+        if(!user){
+            return done(null,false,{message:'Incorrect username.'});
+        }
+        const isPasswordMatch=user.password===password? true : false;
+        if(isPasswordMatch){
+            return done(null,user);
+        }else{
+            return done (null,false,{message: 'Incorrect password.'});
+        }
+    }catch(err){
+        return done(err);
+    }
+}));
+
+module.exports=passport;//export configure passport
