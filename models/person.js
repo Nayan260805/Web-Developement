d3a532c2a702ba4d370c3394d6471663710ce568
Nayan Pillewar <nayanpillewar7@gmail.com>
2025-03-14 23:51:09 +0530

    Authentication Authorization2

--- models/person.js
+++ models/person.js
@@ -1,5 +1,7 @@
 const { default: mongoose } = require('mongoose');
 const mongooes= require('mongoose');
+
+const bcrypt = require('bcrypt');
 //Define the person Schema
 const personSchema = new mongoose.Schema({
     name:{
@@ -28,7 +30,45 @@ const personSchema = new mongoose.Schema({
     }
 });
 
-//create person model
+personSchema.pre('save',async function(next){
+    const Person=this;
+
+    //hash the password only if it has been modified (or is new)
+    if(!Person.isModified('password')) return next();
+    try{
+        //hash password generation 
+        const salt=await bcrypt.genSalt(10);//is responsible for generating a salt
+
+        //hash password 
+        const hashPassword= await bcrypt.hash(Person.password,salt);
+
+        //Override the plain password with the hashed one
+        Person.password=hashPassword;
+
+        next();//call back function
+    }catch(err){
+        return next(err);
+
+    }
+})
+
+personSchema.methods.comparePassword=async function (candidatePassword){
+    try{
+        //use bcrypt to compare the provide password with the hashed password
+        const isMatch = await bcrypt.compare(candidatePassword,this.password);
+        return isMatch;
+
+    }catch(err){
+        throw err;
+    }
+}
+
+// nayan ----> bdggwjbtjdcjhdakjdh
+// login --> nehal
+//compare function bdggwjbtjdcjhdakjdh --->nayan comapre function extract salt from password
+// salt+nehal --->hash ---> swnnfceretnfjhfdvlew
+//ye joh hash hai swnnfceretnfjhfdvlew == bdggwjbtjdcjhdakjdh is equal
+//create person model 
 
 const person = mongoose.model('person', personSchema);
 module.exports=person;
